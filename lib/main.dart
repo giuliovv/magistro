61fd079a9320895a55aadf7cacabf28a7e650244
Giulio Vaccari <giulio09@marcov.it>
2023-04-13 11:27:29 +0200

    Multiple html pages

--- lib/main.dart
+++ lib/main.dart
@@ -71,6 +71,7 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
+        automaticallyImplyLeading: false,
         // Here we take the value from the MyHomePage object that was created by
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
